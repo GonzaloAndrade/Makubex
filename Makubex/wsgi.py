cdd853868a0fa6b8645b4291d597748601662fd4
Gonzalo Andrade <gandrade@semgroup.ec>
2017-05-01 16:39:44 -0500

    First Commit

---
+++ Makubex/wsgi.py
@@ -0,0 +1,14 @@
+"""
+WSGI config for Makubex project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
+"""
+
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Makubex.settings")
+
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
